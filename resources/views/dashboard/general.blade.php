7e861b09c9f94974fb725eddcdd80741e9ef8a4d
adi2 <adi@pentacode.id>
2024-03-18 22:20:12 +0700

    first install

---
+++ resources/views/dashboard/general.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="statbox widget box box-shadow">
+    <div class="widget-header">
+        <div class="row">
+            <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                <h4>Input Text</h4>
+            </div>                 
+        </div>
+    </div>
+    <div class="widget-content widget-content-area">
+
+        <div class="row">
+            <div class="col-lg-6 col-12 ">
+                <form method="post">
+                    <div class="form-group">
+                        <p>Use input <code>type="text"</code>.</p>
+                        <label for="t-text" class="visually-hidden">Text</label>
+                        <input id="t-text" type="text" name="txt" placeholder="Some Text..." class="form-control" required="">
+                        <input type="submit" name="txt" class="mt-4 btn btn-primary">
+                    </div>
+                </form>
+            </div>                                        
+        </div>
+
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard/general', [DashboardController::class, 'general'])->name('dashboard.general');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
